b5cf3a0e089997159cbe6c4ccf47c56a0bea5321
Gabriele Falloon <ggfalloon@gmail.com>
2020-12-22 11:46:18 -0600

    passed tests

--- app.js
+++ app.js
@@ -14,6 +14,62 @@ const render = require("./lib/htmlRenderer");
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
+class Employee {
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+
+    getName() {
+
+
+    }
+
+    getId() {
+
+
+    }
+    getEmail() {
+
+
+    }
+
+    getRole() {
+
+        return Employee;
+    }
+}
+
+class Manager {
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+
+    getName() {
+
+
+    }
+
+    getId() {
+
+
+    }
+    getEmail() {
+
+
+    }
+
+    getRole() {
+
+        return Employee;
+    }
+}
+
+
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
